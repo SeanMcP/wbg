83992f2a95cd54909e2493343ad9670e789aeb4b
Sean McPherson <git@seanmcp.com>
2020-02-13 13:34:04 -0500

    Tinker and port from seanmcp.com

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
+const metadata = require('./_data/metadata.json')
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
     strict_filters: true
   });
-  
+
   eleventyConfig.addLayoutAlias("base", "layouts/base.liquid");
+
+  eleventyConfig.addFilter('absoluteUrl', url => (
+    `${metadata.url}${url.slice(1)}`
+  ))
+
+  const directoriesToCopyToBuild = ['css']
+  directoriesToCopyToBuild.forEach(dir => eleventyConfig.addPassthroughCopy(dir))
 
   return {
     templateFormats: ["liquid", "md"],
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ### Liquid
 
 Chosen because it was a) _not_ Nunjucks, and b) has a lot of available features.
+
+## Design
+
+### Palette
+
+I kinda like this one: [https://colorhunt.co/palette/158095]
